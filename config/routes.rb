f785bb0bc3cf223f9ebc97a4b869df889087bdf5
Paula Kusinska <paula.kusinska@gmail.com>
2020-01-14 21:58:27 +0900

    Added controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails do
-    resources :doses, only: [:index, :new, :create, :show]
-    resources :ingredients, only: [:index, :new, :create, :show]
+    resources :doses, only: %i[new create show]
+    resources :ingredients, only: [:new, :create, :show]
   end
+  resources :doses, only: :destroy
 end
