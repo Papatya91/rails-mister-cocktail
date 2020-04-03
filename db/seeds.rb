c655f3e57491b904c9b5c24adfbb02450b5356de
Paula Kusinska <paula.kusinska@gmail.com>
2020-04-03 18:00:18 +0900

    fixed the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,17 +12,5 @@
 require 'json'
 require 'open-uri'
 
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-ingredients_serialized = open(url).read
-ingredients_json = JSON.parse(ingredients_serialized)
-ingredients = ingredients_json["drinks"]
-ingredients.each do |ingredient|
-  Ingredient.create(name: ingredient["strIngredient1"])
-end
+cocktails = Cocktail.create([{ name: 'Malibu' }, { name: 'Caipirihna' }, { name: 'Moscow mule'},{ name: 'Vodka sour' }, { name: 'Gin tonic' }, { name: 'Vodka mix' }]);
 
-Cocktail.create(name: 'Malibu')
-Cocktail.create(name: 'Caipirihna')
-Coktail.create(name: 'Moscow mule')
-Cocktail.create(name: 'Vodka sour')
-Coktail.create(name: 'Gin tonic')
-Cocktail.create(name: 'Vodka mix')
